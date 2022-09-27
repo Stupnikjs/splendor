a952096852345fb18f31aca20e410300ec95acaa
stupnikk <andre.lacassagne82@gmail.com>
2022-09-27 21:20:33 +0200

    style

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const PlayerCard = (props) => {
     return (
-       <div className='playerCard'>
-        <li className={props.color}></li><span>{props.player[1].filter(element => element.color === props.color ).length}</span>
+       <div className={'playerCard' + ' ' + props.color} >
+        <span>{props.player[1].filter(element => element.color === props.color ).length}</span>
        </div> 
     );
 };
